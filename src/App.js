eada63433a4b2886599ff6a7657956419eb663da
rio0810 <ryoji.motani@gmail.com>
2022-06-28 15:13:15 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -2,15 +2,32 @@ import "./App.css";
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
+import { HeaderOnly } from "./components/templates/HeaderOnly";
+import { BrowserRouter } from "react-router-dom";
+
+const user = {
+  name: "rio",
+  image: "https://source.unsplash.com/gKXKBY-C-Dk",
+  email: "example.com",
+  phone: "090-0000-1111",
+  company: {
+    name: "テスト株式会社",
+  },
+  website: "https://google.com",
+};
 
 function App() {
   return (
-    <div className="App">
-      <PrimaryButton>テスト</PrimaryButton>
-      <SecondaryButton>検索</SecondaryButton>
-      <br />
-      <SearchInput />
-    </div>
+    <BrowserRouter>
+      <HeaderOnly>
+        <PrimaryButton>テスト</PrimaryButton>
+        <SecondaryButton>検索</SecondaryButton>
+        <br />
+        <SearchInput />
+        <UserCard user={user} />
+      </HeaderOnly>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/templates/HeaderOnly.jsx
@@ -0,0 +1,4 @@
+export const HeaderOnly = (props) => {
+  const { children } = props;
+  return <>{children}</>;
+};
